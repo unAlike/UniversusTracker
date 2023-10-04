80a0d39dd3c980388ffcc30719daa4f10391bc9b
unAlike <Ryan.Ungerleider@gmail.com>
2023-10-03 20:01:18 -0500

    Added Players HP

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 function App() {
   const [DMG, setDMG] = useState(0);
   const [speed, setSpeed] = useState(0);
+  const [p1, setP1] = useState(0);
+  const [p2, setP2] = useState(0);
 
   const addDmg = () => {
     setDMG((prev) => prev + 1);
@@ -22,12 +24,27 @@ function App() {
     setSpeed((prev) => prev - 1);
   }
 
-
+  const addP1 = () => {
+    setP1((prev) => prev+1);
+  }
+  const remP1 = () => {
+    setP1((prev) => prev-1);
+  }
+  const addP2 = () => {
+    setP2((prev) => prev+1);
+  }
+  const remP2 = () => {
+    setP2((prev) => prev-1);
+  }
 
   return (
     <div className="AppHeader">
         <div className="topDiv">
-        
+          <div className="addHP" onClick={addP1}/>
+          <div className="HP">
+            {p1}
+          </div>
+          <div className="remHP" onClick={remP1}/>
         </div>
         <div className="midDiv">
             <div className="minusSpeed"  onClick={remSpeed}>
@@ -55,7 +72,11 @@ function App() {
 
           </div>
         <div className="botDiv">
-
+          <div className="addHP" onClick={addP2}/>
+          <div className="HP">
+            {p2}
+          </div>
+          <div className="remHP" onClick={remP2}/>
         </div>
     </div>
   );
